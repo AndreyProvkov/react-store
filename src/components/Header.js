6f441babcb72fc8d64da4521f40affde34a8ec56
Andrey Provkov <provkovasu15@gmail.com>
2022-07-01 14:38:40 +0500

    Add bascket; add styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa"
 
 export default function Header() {
+  let [bascketOpen, setBascketOpen] = useState(false);
+
   return (
     <header className='header'>
         <div className='header__name'>Name Cmpny</div>
-        <ul className='header__menu menu'>
+        <nav className='header__nav'>
+          <ul className='header__menu menu'>
             <li className='menu__item'>First item</li>
             <li className='menu__item'>Second item</li>
             <li className='menu__item'>Third item</li>
-        </ul>
+          </ul>
+          <FaShoppingCart onClick={() => setBascketOpen(bascketOpen = !bascketOpen)} className={`bascket ${bascketOpen && 'bascket_active'}`} />
+        </nav>
+        {bascketOpen && (<div className='bascket__window'></div>)}
     </header>
   )
 }
